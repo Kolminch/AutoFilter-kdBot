30f6796a2d2d8d3a6c66ac7b4c16c6cbfd24826b
Kolminch <122073398+Kolminch@users.noreply.github.com>
2023-02-05 23:26:57 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -1079,7 +1079,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    text=f"[📥{get_size(file.file_size)}📥] {file.file_name}", 
                     url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
             ]
