5e857f571f2b718f09f33b023303a8267072e19f
Kolminch <122073398+Kolminch@users.noreply.github.com>
2023-02-08 23:03:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -484,7 +484,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://Fire-links.in/api'
+        url = f'https://{URL_SHORTNER_WEBSITE}/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
