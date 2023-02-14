7feabdd45e3106d5af2ac35d4d3dccf08e11efd5
Kolminch <122073398+Kolminch@users.noreply.github.com>
2023-02-14 18:21:09 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -1065,7 +1065,7 @@ async def auto_filter(client, msg, spoll=False):
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
                 if settings["spell_check"]:
-                    return await advantage_spell_chok(msg)
+                    return await advantage_spell_check(msg)
                 else:
                     return
         else:
@@ -1282,14 +1282,14 @@ async def auto_filter(client, msg, spoll=False):
     if spoll:
         await msg.message.delete()
 
-async def advantage_spell_chok(msg):
+async def advantage_spell_check(msg):
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
     search = msg.text.replace(" ", "+")
     query = query.strip() + " movie"
-    g_s = await search_gagala(query)
-    g_s += await search_gagala(msg.text)
+    g_s = await search_google(query)
+    g_s += await search_google(msg.text)
     gs_parsed = []
     if not g_s:
         btn = [[
