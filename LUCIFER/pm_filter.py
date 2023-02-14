3d06262232cf19a2dd6575648ca4c91e915486f3
Kolminch <122073398+Kolminch@users.noreply.github.com>
2023-02-14 16:58:46 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -536,7 +536,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('➕ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('🍁 Oᴡɴᴇʀ', callback_data="owner_info"),
-                    InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
+                    InlineKeyboardButton('ꜱʜᴀʀᴇ ʙᴏᴛ', url='https://telegram.me/share/url?url=https://bit.ly/3E19G7l)
                 ],[
                     InlineKeyboardButton('❗ Hᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('🕵️ Aʙᴏᴜᴛ', callback_data='about'),
