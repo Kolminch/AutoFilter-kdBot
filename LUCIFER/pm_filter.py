c0a9950f8769c0e2ae1fe3567ae6887457509523
Adheei <108084282+Adheei@users.noreply.github.com>
2023-02-14 12:14:14 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -34,7 +34,7 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message(filters.private & filters.text & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming & filters.user)
+@Client.on_message(filters.private & filters.text & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.private & filters.text & filters.incoming)
 async def pv_filter(client, message):
     kd = await global_filters(client, message)
     if kd == False:
